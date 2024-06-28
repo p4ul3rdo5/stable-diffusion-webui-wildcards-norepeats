ecdf031f75daaaafd2b8f6ac426871e015e8c3ca
p4ul3rdo5 <p4ul3rdo5@protonmail.com>
2024-06-28 03:30:11 +0200

    Adding no-repeat feature

--- scripts/wildcards.py
+++ scripts/wildcards.py
@@ -1,12 +1,12 @@
 import os
 import random
 import sys
+import re
 
 from modules import scripts, script_callbacks, shared
 
 warned_about_files = {}
 repo_dir = scripts.basedir()
-
 
 class WildcardsScript(scripts.Script):
     def title(self):
@@ -15,31 +15,37 @@ class WildcardsScript(scripts.Script):
     def show(self, is_img2img):
         return scripts.AlwaysVisible
 
-    def replace_wildcard(self, text, gen):
-        if " " in text or len(text) == 0:
-            return text
-
-        wildcards_dir = shared.cmd_opts.wildcards_dir or os.path.join(repo_dir, "wildcards")
-
-        replacement_file = os.path.join(wildcards_dir, f"{text}.txt")
-        if os.path.exists(replacement_file):
-            with open(replacement_file, encoding="utf8") as f:
-                return gen.choice(f.read().splitlines())
-        else:
-            if replacement_file not in warned_about_files:
-                print(f"File {replacement_file} not found for the __{text}__ wildcard.", file=sys.stderr)
-                warned_about_files[replacement_file] = 1
-
-        return text
-
     def replace_prompts(self, prompts, seeds):
         res = []
-
+        wildcards_dir = shared.cmd_opts.wildcards_dir or os.path.join(repo_dir, "wildcards")
         for i, text in enumerate(prompts):
             gen = random.Random()
             gen.seed(seeds[0 if shared.opts.wildcards_same_seed else i])
+            
+            cards = set(re.findall(r'__(.*?)__', text))
+            
+            replacements = {}
+            for card in cards:           
+                replacement_file = os.path.join(wildcards_dir, f"{card}.txt")
+                if os.path.exists(replacement_file):
+                    with open(replacement_file, encoding="utf8") as f:
+                        replacements[card]=f.read().splitlines()
+                        random.shuffle(replacements[card])
+                        if not shared.opts.wildcards_allow_repeats:
+                            replacements[card] = list(dict.fromkeys(replacements[card])) 
+                else:
+                    if replacement_file not in warned_about_files:
+                        print(f"File {replacement_file} not found for the __{text}__ wildcard.", file=sys.stderr)
+                        warned_about_files[replacement_file] = 1
+                        
+            
+            def replace_key(match):
+                card = match.group(1)
+                if card in replacements and replacements[card]:
+                    return replacements[card].pop(0)
+                return ""
 
-            res.append("".join(self.replace_wildcard(chunk, gen) for chunk in text.split("__")))
+            res.append(re.sub(r'__(.*?)__', replace_key, text))
 
         return res
 
@@ -56,6 +62,6 @@ class WildcardsScript(scripts.Script):
 
 def on_ui_settings():
     shared.opts.add_option("wildcards_same_seed", shared.OptionInfo(False, "Use same seed for all images", section=("wildcards", "Wildcards")))
-
+    shared.opts.add_option("wildcards_allow_repeats", shared.OptionInfo(False, "Allow repeating values if using the same wildcard twice", section=("wildcards", "Wildcards")))
 
 script_callbacks.on_ui_settings(on_ui_settings)
